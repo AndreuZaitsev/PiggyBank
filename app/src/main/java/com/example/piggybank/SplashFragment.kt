3a06799373f0802732a99455003b2a0a7e8238b9
Andreu Zaitsev <zaytssss@mail.ru>
2023-04-16 16:53:21 +0300

    Create main screen Add navigation to main

--- app/src/main/java/com/example/piggybank/SplashFragment.kt
+++ app/src/main/java/com/example/piggybank/SplashFragment.kt
@@ -1,6 +1,30 @@
 package com.example.piggybank
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
-class SplashFragment: Fragment(R.layout.splash_fragment) {
+class SplashFragment : Fragment(R.layout.splash_fragment) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        scheduleNavigationToMain()
+    }
+
+    private fun scheduleNavigationToMain() {
+        val currentTime = System.currentTimeMillis()
+        val isTimePassed = { (System.currentTimeMillis() - currentTime) >= 4000 }
+        Thread {
+            while (true) {
+                if (isTimePassed()) {
+                    break
+                }
+            }
+            activity?.runOnUiThread {
+                findNavController().navigate(R.id.action_splashFragment_to_mainFragment)
+            }
+
+        }.start()
+    }
 }
